02cbc79dd101a8d8e98736c564e735e4ee17593e
sebastian <sebastian@sebastian-ThinkPad-W530>
2014-02-26 17:17:58 +0100

    TransortCanDataFrame

--- src/applications/can_flexray/CanSourceGatewayAppBase.cc
+++ src/applications/can_flexray/CanSourceGatewayAppBase.cc
@@ -13,129 +13,130 @@
 // along with this program.  If not, see http://www.gnu.org/licenses/.
 // 
 
-//#include "CanSourceGatewayAppBase.h"
-//#include "TransportMessage_m.h"
-//
-//Define_Module(CanSourceGatewayAppBase);
-//
-//void CanSourceGatewayAppBase::initialize() {
-//    canVersion = getParentModule()->par("version").stdstringValue();
-//    //initialDataFrameCreation();
-//    //initialRemoteFrameCreation();
-//}
-//
-//void CanSourceGatewayAppBase::handleMessage(cMessage *msg) {
-//    //CanDataFrame *df = check_and_cast<CanDataFrame *>(msg);
-//    //dataFrameTransmission(df);
-//    if(msg->arrivedOn("routingIn")){
-//        TransportMessage *transFrame = dynamic_cast<TransportMessage*>(msg);
-//        CanDataFrame *dataFrame = check_and_cast<CanDataFrame *>(transFrame->decapsulate());
-//        dataFrame->setStartTime(simTime());
-//        send(dataFrame, "out");
-//    }
-//}
-//
-//void CanSourceGatewayAppBase::initialRemoteFrameCreation() { //TODO was, wenn keine frames vorhanden?
-//    /*
-//    if (getParentModule()->par("idRemoteFrames").stdstringValue() != "0") {
-//        cStringTokenizer remoteFrameIDsTokenizer(
-//                getParentModule()->par("idRemoteFrames"), ",");
-//        vector<int> remoteFrameIDs = remoteFrameIDsTokenizer.asIntVector();
-//        cStringTokenizer remoteFramesPeriodicityTokenizer(
-//                getParentModule()->par("periodicityRemoteFrames"), ",");
-//
-//        cStringTokenizer dataLengthRemoteFramesTokenizer(
-//                getParentModule()->par("dataLengthRemoteFrames"), ",");
-//
-//        for (unsigned int i = 0; i < remoteFrameIDs.size(); i++) {
-//            CanDataFrame *can_msg = new CanDataFrame("remoteFrame");
-//            can_msg->setNode(getParentModule()->par("node"));
-//            can_msg->setCanID(checkAndReturnID(remoteFrameIDs.at(i)));
-//            can_msg->setLength(
-//                    calculateLength(
-//                            atoi(dataLengthRemoteFramesTokenizer.nextToken())));
-//            can_msg->setRtr(true);
-//            can_msg->setPeriod(
-//                    atoi(remoteFramesPeriodicityTokenizer.nextToken()));
-//            if (can_msg->getPeriod() == 0) {
-//                EV<<"Remote frame with ID "<< can_msg->getCanID() << " has no period. Hence it will be ignored.\n";
-//            } else {
-//                outgoingRemoteFrames.push_back(can_msg); // TODO brauch ich das? ich glaube nicht
-//                scheduleAt(simTime() + (can_msg->getPeriod() / 1000.), can_msg);
-//            }
-//        }
-//    }
-//    */
-//}
-//
-//void CanSourceGatewayAppBase::initialDataFrameCreation() { //TODO was, wenn keine frames vorhanden?
-//    /*if (getParentModule()->par("idDataFrames").stdstringValue() != "0") {
-//        cStringTokenizer dataFrameIDsTokenizer(
-//                getParentModule()->par("idDataFrames"), ",");
-//        vector<int> dataFrameIDs = dataFrameIDsTokenizer.asIntVector();
-//
-//        cStringTokenizer dataFramesPeriodicityTokenizer(
-//                getParentModule()->par("periodicityDataFrames"), ",");
-//
-//        cStringTokenizer dataLengthDataFramesTokenizer(
-//                getParentModule()->par("dataLengthDataFrames"), ",");
-//
-//        for (unsigned int i = 0; i < dataFrameIDs.size(); i++) {
-//            CanDataFrame *can_msg = new CanDataFrame("message");
-//            can_msg->setNode(getParentModule()->par("node"));
-//            can_msg->setCanID(checkAndReturnID(dataFrameIDs.at(i)));
-//            can_msg->setLength(
-//                    calculateLength(
-//                            atoi(dataLengthDataFramesTokenizer.nextToken())));
-//            can_msg->setPeriod(
-//                    atoi(dataFramesPeriodicityTokenizer.nextToken()));
-//            outgoingDataFrames.push_back(can_msg); // TODO brauch ich das? ich glaube schon
-//            if (can_msg->getPeriod() != 0) {
-//                scheduleAt(simTime() + (can_msg->getPeriod() / 1000.), can_msg);
-//            }
-//        }
-//    }*/
-//}
-//
-//int CanSourceGatewayAppBase::checkAndReturnID(int id) {
-//    if (canVersion.compare("2.0A") == 0) {           //2.0A
-//        if (id < 0 || id > VERSIONAMAX) {
-//            EV<< "ID " << id << " not valid." << endl;
-//            endSimulation();
-//        }
-//    } else {                                    //2.0B
-//        if(id < 0 || id > VERSIONBMAX) {
-//            EV << "ID " << id << " not valid." << endl;
-//            endSimulation();
-//        }
-//    }
-//    return id;
-//}
-//
-//int CanSourceGatewayAppBase::calculateLength(int dataLength) {
-//    int frameLength = 0;
-//    if (canVersion.compare("2.0B") == 0) {
-//        frameLength += ARBITRATIONFIELD29BIT;
-//    }
-//    return frameLength + DATAFRAMEOVERHEAD + (dataLength << 3); //TODO + StuffingBits
-//}
-//
-//void CanSourceGatewayAppBase::dataFrameTransmission(CanDataFrame *df) {
-//    CanDataFrame *outgoingFrame;
-//    if (df->isSelfMessage()) {
-//        outgoingFrame = df->dup();
-//        scheduleAt(simTime() + (df->getPeriod() / 1000.), df);
-//    } else if (df->arrivedOn("remoteIn")) {
-//        for (std::vector<CanDataFrame*>::iterator it = outgoingDataFrames.begin();
-//                it != outgoingDataFrames.end(); ++it) {
-//            CanDataFrame *tmp = *it;
-//            if (tmp->getCanID() == df->getCanID()) {
-//                outgoingFrame = tmp->dup();
-//                break;
-//            }
-//        }
-//        delete df;
-//    }
-//    outgoingFrame->setStartTime(simTime());
-//    send(outgoingFrame, "out");
-//}
+#include "CanSourceGatewayAppBase.h"
+#include "TransportMessage_m.h"
+#include "candataframe_m.h"
+
+Define_Module(CanSourceGatewayAppBase);
+
+void CanSourceGatewayAppBase::initialize() {
+    canVersion = getParentModule()->par("version").stdstringValue();
+    //initialDataFrameCreation();
+    //initialRemoteFrameCreation();
+}
+
+void CanSourceGatewayAppBase::handleMessage(cMessage *msg) {
+    //CanDataFrame *df = check_and_cast<CanDataFrame *>(msg);
+    //dataFrameTransmission(df);
+    if(msg->arrivedOn("routingIn")){
+        TransportMessage *transFrame = check_and_cast<TransportMessage*>(msg);
+        //CanDataFrame *dataFrame = check_and_cast<CanDataFrame *>(transFrame->decapsulate());
+        //dataFrame->setStartTime(simTime());
+        //send(dataFrame, "out");
+    }
+}
+
+void CanSourceGatewayAppBase::initialRemoteFrameCreation() { //TODO was, wenn keine frames vorhanden?
+    /*
+    if (getParentModule()->par("idRemoteFrames").stdstringValue() != "0") {
+        cStringTokenizer remoteFrameIDsTokenizer(
+                getParentModule()->par("idRemoteFrames"), ",");
+        vector<int> remoteFrameIDs = remoteFrameIDsTokenizer.asIntVector();
+        cStringTokenizer remoteFramesPeriodicityTokenizer(
+                getParentModule()->par("periodicityRemoteFrames"), ",");
+
+        cStringTokenizer dataLengthRemoteFramesTokenizer(
+                getParentModule()->par("dataLengthRemoteFrames"), ",");
+
+        for (unsigned int i = 0; i < remoteFrameIDs.size(); i++) {
+            CanDataFrame *can_msg = new CanDataFrame("remoteFrame");
+            can_msg->setNode(getParentModule()->par("node"));
+            can_msg->setCanID(checkAndReturnID(remoteFrameIDs.at(i)));
+            can_msg->setLength(
+                    calculateLength(
+                            atoi(dataLengthRemoteFramesTokenizer.nextToken())));
+            can_msg->setRtr(true);
+            can_msg->setPeriod(
+                    atoi(remoteFramesPeriodicityTokenizer.nextToken()));
+            if (can_msg->getPeriod() == 0) {
+                EV<<"Remote frame with ID "<< can_msg->getCanID() << " has no period. Hence it will be ignored.\n";
+            } else {
+                outgoingRemoteFrames.push_back(can_msg); // TODO brauch ich das? ich glaube nicht
+                scheduleAt(simTime() + (can_msg->getPeriod() / 1000.), can_msg);
+            }
+        }
+    }
+    */
+}
+
+void CanSourceGatewayAppBase::initialDataFrameCreation() { //TODO was, wenn keine frames vorhanden?
+    /*if (getParentModule()->par("idDataFrames").stdstringValue() != "0") {
+        cStringTokenizer dataFrameIDsTokenizer(
+                getParentModule()->par("idDataFrames"), ",");
+        vector<int> dataFrameIDs = dataFrameIDsTokenizer.asIntVector();
+
+        cStringTokenizer dataFramesPeriodicityTokenizer(
+                getParentModule()->par("periodicityDataFrames"), ",");
+
+        cStringTokenizer dataLengthDataFramesTokenizer(
+                getParentModule()->par("dataLengthDataFrames"), ",");
+
+        for (unsigned int i = 0; i < dataFrameIDs.size(); i++) {
+            CanDataFrame *can_msg = new CanDataFrame("message");
+            can_msg->setNode(getParentModule()->par("node"));
+            can_msg->setCanID(checkAndReturnID(dataFrameIDs.at(i)));
+            can_msg->setLength(
+                    calculateLength(
+                            atoi(dataLengthDataFramesTokenizer.nextToken())));
+            can_msg->setPeriod(
+                    atoi(dataFramesPeriodicityTokenizer.nextToken()));
+            outgoingDataFrames.push_back(can_msg); // TODO brauch ich das? ich glaube schon
+            if (can_msg->getPeriod() != 0) {
+                scheduleAt(simTime() + (can_msg->getPeriod() / 1000.), can_msg);
+            }
+        }
+    }*/
+}
+
+int CanSourceGatewayAppBase::checkAndReturnID(int id) {
+    if (canVersion.compare("2.0A") == 0) {           //2.0A
+        if (id < 0 || id > VERSIONAMAX) {
+            EV<< "ID " << id << " not valid." << endl;
+            endSimulation();
+        }
+    } else {                                    //2.0B
+        if(id < 0 || id > VERSIONBMAX) {
+            EV << "ID " << id << " not valid." << endl;
+            endSimulation();
+        }
+    }
+    return id;
+}
+
+int CanSourceGatewayAppBase::calculateLength(int dataLength) {
+    int frameLength = 0;
+    if (canVersion.compare("2.0B") == 0) {
+        frameLength += ARBITRATIONFIELD29BIT;
+    }
+    return frameLength + DATAFRAMEOVERHEAD + (dataLength << 3); //TODO + StuffingBits
+}
+
+void CanSourceGatewayAppBase::dataFrameTransmission(CanDataFrame *df) {
+    CanDataFrame *outgoingFrame;
+    if (df->isSelfMessage()) {
+        outgoingFrame = df->dup();
+        scheduleAt(simTime() + (df->getPeriod() / 1000.), df);
+    } else if (df->arrivedOn("remoteIn")) {
+        for (std::vector<CanDataFrame*>::iterator it = outgoingDataFrames.begin();
+                it != outgoingDataFrames.end(); ++it) {
+            CanDataFrame *tmp = *it;
+            if (tmp->getCanID() == df->getCanID()) {
+                outgoingFrame = tmp->dup();
+                break;
+            }
+        }
+        delete df;
+    }
+    outgoingFrame->setStartTime(simTime());
+    send(outgoingFrame, "out");
+}
--- src/applications/can_flexray/CanSourceGatewayAppBase.h
+++ src/applications/can_flexray/CanSourceGatewayAppBase.h
@@ -11,102 +11,102 @@
 // 
 // You should have received a copy of the GNU Lesser General Public License
 // along with this program.  If not, see http://www.gnu.org/licenses/.
-// 
-//
-//#ifndef __SIGNALSANDGATEWAYS_CANSOURCEGATEWAYAPPBASE_H_
-//#define __SIGNALSANDGATEWAYS_CANSOURCEGATEWAYAPPBASE_H_
-//
-//#include <omnetpp.h>
-//#include <string.h>
-//#include "CanBuffer.h"
-//#include "candataframe_m.h"
-//
-///**
-// * TODO - Generated class
-// */
-//class CanSourceGatewayAppBase : public cSimpleModule
-//{
-//protected:
-//    /**
-//      * @brief Initialization of the module.
-//      */
-//     virtual void initialize();
-//
-//     /**
-//      * @brief Self messages are processed and outgoing frames are built.
-//      *
-//      * @param msg incoming self message
-//      */
-//     virtual void handleMessage(cMessage *msg);
-//
-// private:
-//     /**
-//      * @brief Overhead for a data frame.
-//      *
-//      * The variable includes Start of frame, arbitration field (11 Bit), remote transmission bit,
-//      * identifier extension bit, reserved bit, data length code, CRC, CRC-delimiter, ACK-slot,
-//      * ACK-delimiter, end of frame and inter frame space.
-//      */
-//     static const int DATAFRAMEOVERHEAD = 47;
-//
-//     /**
-//      * @brief Additional bit needed for the 29 bit ID.
-//      */
-//     static const int ARBITRATIONFIELD29BIT = 20;
-//
-//     /**
-//      * @brief Maximum ID value for Version 2.0A.
-//      */
-//     static const int VERSIONAMAX = 2047;
-//     /**
-//      * @brief Maximum ID value for Version 2.0B.
-//      */
-//     static const int VERSIONBMAX = 536870911;
-//
-//     /**
-//      * @brief The version of CAN used in this network. 2.0A or 2.0B
-//      */
-//     string canVersion;
-//
-//     /**
-//      *
-//      */
-//     int currentFrameID;
-//
-//     /**
-//      * @brief Collection including all
-//      */
-//     vector<CanDataFrame*> outgoingRemoteFrames;
-//
-//     /**
-//      * @brief Creates a data frame which will be queued in the buffer.
-//      */
-//     void initialRemoteFrameCreation();
-//
-//     /**
-//      * @brief Collection including all
-//      */
-//     vector<CanDataFrame*> outgoingDataFrames;
-//
-//     /**
-//      * @brief Creates a data frame which will be queued in the buffer.
-//      */
-//     void initialDataFrameCreation();
-//
-//     /**
-//      *
-//      */
-//     int checkAndReturnID(int id);
-//
-//     /**
-//      * @brief Calculates the length for the data frame.
-//      */
-//     int calculateLength(int datalength);
-//
-//     /**
-//      * @brief Transmits the data frame to the connected output buffer.
-//      */
-//     void dataFrameTransmission(CanDataFrame *df);
-//};
-//
-//#endif
+
+
+#ifndef __SIGNALSANDGATEWAYS_CANSOURCEGATEWAYAPPBASE_H_
+#define __SIGNALSANDGATEWAYS_CANSOURCEGATEWAYAPPBASE_H_
+
+#include <omnetpp.h>
+#include <string.h>
+#include "buffer/can/Buffer.h"
+
+
+/**
+ * TODO - Generated class
+ */
+class CanSourceGatewayAppBase : public cSimpleModule
+{
+protected:
+    /**
+      * @brief Initialization of the module.
+      */
+     virtual void initialize();
+
+     /**
+      * @brief Self messages are processed and outgoing frames are built.
+      *
+      * @param msg incoming self message
+      */
+     virtual void handleMessage(cMessage *msg);
+
+ private:
+     /**
+      * @brief Overhead for a data frame.
+      *
+      * The variable includes Start of frame, arbitration field (11 Bit), remote transmission bit,
+      * identifier extension bit, reserved bit, data length code, CRC, CRC-delimiter, ACK-slot,
+      * ACK-delimiter, end of frame and inter frame space.
+      */
+     static const int DATAFRAMEOVERHEAD = 47;
+
+     /**
+      * @brief Additional bit needed for the 29 bit ID.
+      */
+     static const int ARBITRATIONFIELD29BIT = 20;
+
+     /**
+      * @brief Maximum ID value for Version 2.0A.
+      */
+     static const int VERSIONAMAX = 2047;
+     /**
+      * @brief Maximum ID value for Version 2.0B.
+      */
+     static const int VERSIONBMAX = 536870911;
+
+     /**
+      * @brief The version of CAN used in this network. 2.0A or 2.0B
+      */
+     string canVersion;
+
+     /**
+      *
+      */
+     int currentFrameID;
+
+     /**
+      * @brief Collection including all
+      */
+     vector<CanDataFrame*> outgoingRemoteFrames;
+
+     /**
+      * @brief Creates a data frame which will be queued in the buffer.
+      */
+     void initialRemoteFrameCreation();
+
+     /**
+      * @brief Collection including all
+      */
+     vector<CanDataFrame*> outgoingDataFrames;
+
+     /**
+      * @brief Creates a data frame which will be queued in the buffer.
+      */
+     void initialDataFrameCreation();
+
+     /**
+      *
+      */
+     int checkAndReturnID(int id);
+
+     /**
+      * @brief Calculates the length for the data frame.
+      */
+     int calculateLength(int datalength);
+
+     /**
+      * @brief Transmits the data frame to the connected output buffer.
+      */
+     void dataFrameTransmission(CanDataFrame *df);
+};
+
+#endif
