0ce3b1813e446d1a198efce465c63fd1e6e2c732
Till Steinbach <till.steinbach@informatik.haw-hamburg.de>
2014-12-19 16:56:30 +0100

    now fixed leak?

--- src/Utility.cc
+++ src/Utility.cc
@@ -35,13 +35,13 @@ namespace UTLTY{
 
     std::string Utility::stripNonAlphaNum(const char *str)
     {
-        std::string stringRepresentation;// = new std::string(str, 0, 55);
+        std::string stringRepresentation = std::string(str, 0, 55);
         stripNonAlphaNum(stringRepresentation);
         return stringRepresentation;
     }
 
     std::string Utility::stripNonAlphaNum(const char *str, int maxLength){
-        std::string stringRepresentation;// = new std::string(str, findFirstAlphaNum(str), maxLength);
+        std::string stringRepresentation = std::string(str, findFirstAlphaNum(str), maxLength);
         stripNonAlphaNum(stringRepresentation);
         return stringRepresentation;
     }
