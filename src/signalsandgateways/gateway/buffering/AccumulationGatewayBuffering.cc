e77354c506a85e6716c223d1cd72310507fce17f
Philipp Meyer <philipp.meyer@haw-hamburg.de>
2018-11-09 16:22:14 +0100

    Fix Warnings

--- src/signalsandgateways/gateway/buffering/AccumulationGatewayBuffering.cc
+++ src/signalsandgateways/gateway/buffering/AccumulationGatewayBuffering.cc
@@ -173,7 +173,7 @@ void AccumulationGatewayBuffering::emitSignals(cMessagePointerList* poolList) {
             emit(poolHoldUpTimeSignals[poolID], holdUpTime);
         }
         poolArrivalTimes[poolList].clear();
-        emit(poolSizeSignals[poolID], static_cast<unsigned long>(poolList->size()));
+        emit(poolSizeSignals[poolID], poolList->size());
     }
 }
 
